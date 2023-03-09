9f3d1265dd29292fb5f1d808237834ad3f51ef5a
Aleksandra <aleksandraleszczynska@hotmail.com>
2023-03-09 21:00:12 +0100

    [refactor] - posibility to use db connection pool

--- src/main/java/pl/sda/orange2/jdbc/config/H2Config.java
+++ src/main/java/pl/sda/orange2/jdbc/config/H2Config.java
@@ -3,7 +3,7 @@ package pl.sda.orange2.jdbc.config;
 public class H2Config {
     //TYP WYLICZENIOWY
     // JAK NALEZY GO STOSOWAC?
-    public final static String DB_URL = "jdbc:h2:~/databases/jdbc";
+    public final static String DB_URL = "jdbc:h2:tcp://localhost/~/databases/jdbc";
     // pole statyczne od pola instancyjnego rozni siev
     public final static String USER = "sa";
     public final static String PASSWORD = "sa";
